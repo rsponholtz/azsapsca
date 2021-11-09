23e8d197ab3eb91df691ec9374582a361da28c44
Ross Sponholtz <rsponholtz@hotmail.com>
2021-11-08 17:43:56 -0800

    changed order of if statement

--- patterns/AZSAP/tcptimestamps.py
+++ patterns/AZSAP/tcptimestamps.py
@@ -6,8 +6,6 @@
 #
 import os
 import Core
-import HAE
-
 
 META_CLASS = "AZSAP"
 META_CATEGORY = "Kernel"
@@ -21,7 +19,6 @@ OTHER_LINKS = "META_LINK_TID=https://docs.microsoft.com/en-us/azure/virtual-mach
 Core.init(META_CLASS, META_CATEGORY, META_COMPONENT, PATTERN_ID, PRIMARY_LINK, OVERALL, OVERALL_INFO, OTHER_LINKS)
 
 try:
-
     fileOpen = "env.txt"
     section = "/sbin/sysctl -a"
     VALUE = -1
@@ -33,14 +30,12 @@ try:
                 RP_LIST = content[line].split('=')
                 VALUE = int(RP_LIST[1].strip())
 
-    if (VALUE == 1):
-        Core.updateStatus(Core.ERROR, "tcp_timestamps should be zero");
-    else:
+    if (VALUE == 0):
         Core.updateStatus(Core.SUCC , "tcp_timestamps is correctly zero");
+    else:
+        Core.updateStatus(Core.ERROR, "tcp_timestamps should be zero");
 
 except Exception as error:
-	Core.updateStatus(Core.ERROR, "Corosync configuration error: " + str(error))
+        Core.updateStatus(Core.ERROR, "Corosync configuration error: " + str(error))
 
 Core.printPatternResults()
-    
-

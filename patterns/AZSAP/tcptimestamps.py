028e7a64ec530847e7f89a227d4ac59aebe0e632
Ross Sponholtz <rsponholtz@hotmail.com>
2021-11-08 18:08:00 -0800

    updated error levels

--- patterns/AZSAP/tcptimestamps.py
+++ patterns/AZSAP/tcptimestamps.py
@@ -12,7 +12,7 @@ META_CATEGORY = "Kernel"
 META_COMPONENT = "Network"
 PATTERN_ID = os.path.basename(__file__)
 PRIMARY_LINK = "META_LINK_TID"
-OVERALL = Core.TEMP
+OVERALL = Core.WARN
 OVERALL_INFO = "NOT SET"
 OTHER_LINKS = "META_LINK_TID=https://docs.microsoft.com/en-us/azure/virtual-machines/workloads/sap/sap-hana-high-availability#manual-deployment"
 
@@ -33,9 +33,9 @@ try:
     if (VALUE == 0):
         Core.updateStatus(Core.SUCC , "tcp_timestamps is correctly zero");
     else:
-        Core.updateStatus(Core.ERROR, "tcp_timestamps should be zero");
+        Core.updateStatus(Core.CRIT, "tcp_timestamps should be zero");
 
 except Exception as error:
-        Core.updateStatus(Core.ERROR, "Corosync configuration error: " + str(error))
+        Core.updateStatus(Core.ERROR, "Outside the network scope: " + str(error))
 
 Core.printPatternResults()
